ef31f9c94c976080320bd352d87db08355527198
NelsonVelandia <nelson.velandia.mt@correo.usa.edu.co>
2021-10-15 18:53:00 -0500

    se modifica el properties con el puerto 80

--- alquiler/src/main/java/com/usa/alquiler/AlquilerApplication.java
+++ alquiler/src/main/java/com/usa/alquiler/AlquilerApplication.java
@@ -1,6 +1,5 @@
 package com.usa.alquiler;
 
-import javax.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
